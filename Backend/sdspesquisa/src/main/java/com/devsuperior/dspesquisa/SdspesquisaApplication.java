c0526af22a0d6ba2f2bb07d92271181bee39fb2a
gustaa23-beep <gutalexandre@hotmail.com>
2020-09-15 13:35:10 -0300

    Project created

---
+++ Backend/sdspesquisa/src/main/java/com/devsuperior/dspesquisa/SdspesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdspesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdspesquisaApplication.class, args);
+	}
+
+}
